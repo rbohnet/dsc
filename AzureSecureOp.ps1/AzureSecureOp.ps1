917957bb116025db8161516a85a44f8cf429617e
Ralph Bohnet <ralph_bohnet@transcanada.com>
2016-01-18 08:43:22 -0700

    Initial checkin

---
+++ AzureSecureOp.ps1/AzureSecureOp.ps1
@@ -0,0 +1,23 @@
+configuration AddLocalUserAcct
+{
+    param
+    (        
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$UserCredentials
+    )
+    
+    Node localhost 
+    {
+        User LocalUserAccount
+        {
+            Username = $UserCredentials.UserName
+            Password = $UserCredentials
+            Disabled = $false
+            Ensure = "Present"
+            FullName = "Local User Account"
+            Description = "Local User Account"
+            PasswordNeverExpires = $true
+        }                                            
+    }
+}
